8a91c1b36cc43c99415fdf0b2e3bcb6c1ce29ee9
Pawel Gniewek <gniewko.pablo@gmail.com>
2019-06-02 12:20:52 -0700

    Frist version of neural fp predictor

---
+++ src/models/nfp.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# Auhor: Pawel Gniewek, 2019-
+
+
+import sys
+import logging
+
+#import numpy as np
+#from rdkit import Chem
+#from rdkit.Chem import AllChem
+#from rdkit import Chem, DataStructs
+
+from predictor import Predictor
+
+import autograd.numpy as np
+import autograd.numpy.random as npr
+
+#from neuralfingerprint import load_data
+#from neuralfingerprint import build_morgan_deep_net
+from neuralfingerprint import build_conv_deep_net
+from neuralfingerprint import normalize_array, adam
+from neuralfingerprint import build_batched_grad
+from neuralfingerprint.util import rmse
+
+from autograd import grad
+
+
+class NfpPredictor(Predictor):
+    """
+    """
+
+    def __init__(self, radius=4, fplength=64):
+        self._name = "NfpRegressor"
+
+        self.model_params = dict(fp_length=fplength,    # Usually neural fps need far fewer dimensions than morgan.
+                    fp_depth=radius, # The depth of the network equals the fingerprint radius.
+                    conv_width=20,   # Only the neural fps need this parameter.
+                    h1_size=128,     # Size of hidden layer of network on top of fps.
+                    h2_size=128,     # Size of hidden layer of network on top of fps.
+                    L2_reg=np.exp(-2))
+       
+        self.train_params = dict(num_iters=500,
+                    batch_size=64,
+                    init_scale=np.exp(-4),
+                    step_size=np.exp(-6))
+
+        # Define the architecture of the network that sits on top of the fingerprints.
+  
+        self.model = (None, None, None)
+        #self._undo_norm = None
+        #self._trained_weights = None
+        #self._pred_fun = None
+
+
+    def fit(self, smiles_list, logS_list, seed=0):
+        train_smiles = [smiles for smiles in smiles_list]
+        train_logS = [logS for logS in logS_list]
+
+        conv_layer_sizes = [self.model_params['conv_width']] * self.model_params['fp_depth']
+        conv_arch_params = {'num_hidden_features' : conv_layer_sizes, 'fp_length' : self.model_params['fp_length'], 'normalize' : 1}
+        net_arch_params = dict(
+            layer_sizes = [self.model_params['fp_length'], self.model_params['h1_size'], self.model_params['h2_size']],  # One hidden layer
+            normalize=True, L2_reg = self.model_params['L2_reg'], nll_func = rmse)
+
+        loss_fun, pred_fun, conv_parser = build_conv_deep_net(conv_arch_params, net_arch_params, self.model_params['L2_reg'])
+        
+        num_weights = len(conv_parser)
+#        predict_func, trained_weights, conv_training_curve = self._train_nn(pred_fun, loss_fun, num_weights, train_smiles, train_logS, 
+#                                                validation_smiles=None, validation_raw_targets=None)
+        
+#        test_predictions = predict_func(test_inputs)
+
+    
+#    def _train_nn(self, pred_fun, loss_fun, num_weights, train_smiles, train_raw_targets, seed=0, validation_smiles=None, validation_raw_targets=None):
+        
+#         """loss_fun has inputs (weights, smiles, targets)"""
+#        print(("Total number of weights in the network:", num_weights))
+
+        init_weights = npr.RandomState(seed).randn(num_weights) * self.train_params['init_scale']
+
+#        num_print_examples = 100
+        train_logS_norm, undo_norm = normalize_array(train_logS)
+#        training_curve = []
+#        def callback(weights, iter):
+#            if iter % 10 == 0:
+#                print(("max of weights", np.max(np.abs(weights))))
+#                train_preds = undo_norm(pred_fun(weights, train_smiles[:num_print_examples]))
+#                cur_loss = loss_fun(weights, train_smiles[:num_print_examples], train_targets[:num_print_examples])
+#                training_curve.append(cur_loss)
+#                print("Iteration", iter, "loss", cur_loss,\
+#                      "train RMSE", rmse(train_preds, train_raw_targets[:num_print_examples]), end=' ')
+#                if validation_smiles is not None:
+#                    validation_preds = undo_norm(pred_fun(weights, validation_smiles))
+#                    print("Validation RMSE", iter, ":", rmse(validation_preds, validation_raw_targets), end=' ')
+
+        # Build gradient using autograd.
+        grad_fun = grad(loss_fun)
+        grad_fun_with_data = build_batched_grad(grad_fun, self.train_params['batch_size'],
+                                            train_smiles, train_logS_norm)
+
+        # Optimize weights.
+        trained_weights = adam(grad_fun_with_data, init_weights, #callback=callback,
+                           num_iters=self.train_params['num_iters'], step_size=self.train_params['step_size'])
+
+         
+        self.model = (undo_norm, trained_weights, pred_fun) 
+#        self._undo_norm = undo_norm
+#        self._trained_weights = trained_weights
+#        self._pred_fun = pred_fun
+#        
+#        def predict_func(new_smiles):
+#            """Returns to the original units that the raw targets were in."""
+#            return undo_norm(pred_fun(trained_weights, new_smiles))
+#    
+#        self.model = predict_func
+
+        #return predict_func, trained_weights, training_curve
+
+
+
+    def predict(self, smiles_list):
+        (undo_norm, trained_weights, pred_fun) = self.model
+        y_pred = undo_norm(pred_fun(trained_weights, smiles_list))
+        return y_pred
+
+    
+if __name__ == "__main__":
+    from model_utils import get_training_data
+
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    smiles_list, logS_list = get_training_data(sys.argv[1])
+    nfp_regression = NfpPredictor()
+    print (nfp_regression.train(smiles_list, logS_list))
+    
