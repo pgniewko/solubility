e47b888a8650354aadc02ad42d23fd77d3699b4b
Pawel Gniewek <gniewko.pablo@gmail.com>
2019-06-27 21:05:59 -0700

    Add logS to .sdf file

--- src/smiles2sdf.py
+++ src/smiles2sdf.py
@@ -8,20 +8,29 @@ from rdkit import Chem
 
 def read_smiles(fname):
     smiles_list = []
+    logS = []
     with open(fname, 'r') as fin:
         for line in fin:
-            smiles = line.rstrip('\n').split(',')[0]
+            pairs = line.rstrip('\n').split(',')#[0]
+            smiles = pairs[0] #line.rstrip('\n').split(',')[0]
+            if len(pairs) > 1:
+                logS.append(float(pairs[1]))
+            
             smiles_list.append(smiles)
    
 
-    return [Chem.MolFromSmiles(x) for x in smiles_list]
+    return [Chem.MolFromSmiles(x) for x in smiles_list], logS
 
 
 if __name__ == "__main__":
     fin = sys.argv[1]
     fout = sys.argv[2]
    
-    mols = read_smiles(fin)
+    mols, logS = read_smiles(fin)
     w = Chem.SDWriter(fout)
-    for m in mols: 
+    for i,m in enumerate(mols):
+        try:
+            m.SetProp('logS', str(logS[i]))
+        except:
+            pass
         w.write(m)
