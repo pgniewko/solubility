2fa572e0b99a61ea8cc514e9e2aec911736a0399
Pawel Gniewek <gniewko.pablo@gmail.com>
2019-09-29 22:35:06 -0700

    Arg parser added.

--- src/make_challenge_prediction.py
+++ src/make_challenge_prediction.py
@@ -2,13 +2,23 @@
 
 import sys
 import logging
+import argparse
 
+from models.rf import RFPredictor
+from models.nfp import NfpPredictor
+from models.esol import ESOLCalculator
 from models.ensemble import EnsemblePredictor
+
 from models.model_utils import get_training_data
 from models.model_utils import get_test_data
 
 
 def save_predictions(test_smiles, results_data, fname):
+    """
+    TODO: other statitics are going to be returned with results_data.
+    results_data has to be a dict.
+    """
+
     num_folds = len(results_data)
     with open(fname, 'w') as fout:
         for i, smiles in enumerate(test_smiles):
@@ -17,13 +27,37 @@ def save_predictions(test_smiles, results_data, fname):
                 fout.write("{},".format(float(results_data[j][i])))
             fout.write('\n')
 
+def main(args):
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    train_smiles, train_logS = get_training_data(args.train_file)
+    test_smiles = get_test_data(args.test_file)
+
+    if args.model == 'esol':
+        production_model = ESOLCalculator()
+    elif args.model == 'rf':
+        production_model = RFPredictor()
+    elif args.model == 'nfp':
+        production_model = NfpPredictor()
+    elif args.model == 'ensemble':
+        production_model = EnsemblePredictor()
+    else:
+        print("Model not recognized: {}".format(args.model))
+        return 1
+
+    results_data = production_model.test(train_smiles, train_logS, test_smiles)
+    save_predictions(test_smiles, results_data, args.out_file)
+
+    return 0
+
+def parse_args():
+    parser = argparse.ArgmentPaser(description=__doc__)
+    parser.add_argument('--train_file', help='Path to the file with the train data')
+    parser.add_argument('--test_file', help='Path to the file with the test data')
+    parser.add_argument('--out_file', help='Path to the output file')
+    parser.add_argument('--model', default='rf', help='Choose the model: [esol, rf, nfp, ensemble]')
+    return parser
 
 if __name__ == "__main__":
+    args = parse_args()
+    sys.exit(main(args))
 
-    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-    train_smiles, logS_list = get_training_data(sys.argv[1])
-    test_smiles = get_test_data(sys.argv[2])
-
-    production_model = EnsemblePredictor(fp='maccs')
-    results_data = production_model.test(train_smiles, logS_list, test_smiles)
-    save_predictions(test_smiles, results_data, sys.argv[3])
