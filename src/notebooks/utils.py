39c947acccb1ebf115d3832c0f512318af264839
Pawel Gniewek <gniewko.pablo@gmail.com>
2019-10-03 21:09:53 -0700

    Initial code cleanup

--- src/notebooks/utils.py
+++ src/notebooks/utils.py
@@ -1,4 +1,9 @@
 """ Utility functions, some of which are borrowed from Pat Walters: https://github.com/PatWalters/metk """
+import math
+import numpy as np
+from scipy.stats import pearsonr
+from scipy.stats import norm
+
 
 def rmse(pred_array, ref_array):
     """
@@ -19,7 +24,25 @@ def mean_absolute_error(pred_array, ref_array):
     """
     return np.mean(np.abs(pred_array - ref_array))
 
-def max_possible_correlation(vals, error=1 / 3.0, method=pearsonr, cycles=1000):
+
+def pearson_confidence(r, num, interval=0.95):
+    """
+    Calculate upper and lower 95% CI for a Pearson r (not R**2)
+    Inspired by https://stats.stackexchange.com/questions/18887
+    :param r: Pearson's R
+    :param num: number of data points
+    :param interval: confidence interval (0-1.0)
+    :return: lower bound, upper bound
+    """
+    stderr = 1.0 / math.sqrt(num - 3)
+    z_score = norm.ppf(interval)
+    delta = z_score * stderr
+    lower = math.tanh(math.atanh(r) - delta)
+    upper = math.tanh(math.atanh(r) + delta)
+    return lower, upper
+
+
+def max_possible_correlation(vals, error=0.6, method=pearsonr, cycles=1000):
     """
     Calculate the maximum possible correlation given a particular experimental error
     Based on Brown, Muchmore, Hajduk http://www.sciencedirect.com/science/article/pii/S1359644609000403
