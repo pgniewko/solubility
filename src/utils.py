a5dc03400f3e47ee7dc6594d0154e1c4ed7ff327
Pawel Gniewek <gniewko.pablo@gmail.com>
2019-10-03 21:03:09 -0700

    Code cleanup

--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,34 @@
-
 import cirpy
 from rdkit import Chem
 from rdkit.Chem.Descriptors import MolWt
 
 
 def mol_wt(smiles):
+    """Get molecular weight (in Daltons)"""
     return MolWt(Chem.MolFromSmiles(smiles))
 
 
 def canonicalize_smiles(smiles, iso=False, SLN=False):
+    """Canonicalize given SMILES string
+    The function is a wrapper around RDKIT function
+
+    :argumnts:
+      smiles -- (string) a compound in SMILES format
+      iso -- (bool) include isomeric data in SMILES
+      SLN -- (bool) is the molecule given in SLN format
+
+    :return:
+      canonicalized SMILES
+    """
     if SLN:
         smiles_ = cirpy.resolve(smiles, 'smiles')
-        return Chem.MolToSmiles(Chem.MolFromSmiles(smiles_), canonical=True, isomericSmiles=iso)
+        return Chem.MolToSmiles(Chem.MolFromSmiles(smiles_),
+                                canonical=True,
+                                isomericSmiles=iso)
     else:
-        return Chem.MolToSmiles(Chem.MolFromSmiles(smiles), canonical=True, isomericSmiles=iso)
+        return Chem.MolToSmiles(Chem.MolFromSmiles(smiles),
+                                canonical=True,
+                                isomericSmiles=iso)
 
 
 if __name__ == "__main__":
