42fb53c409c73af502df90379b383157e4c62884
Pawel Gniewek <gniewko.pablo@gmail.com>
2019-10-05 18:41:06 -0700

    Removed redundant code

--- src/notebooks/utils.py
+++ src/notebooks/utils.py
@@ -5,26 +5,6 @@ from scipy.stats import pearsonr
 from scipy.stats import norm
 
 
-def rmse(pred_array, ref_array):
-    """
-    Calculate root mean squared (rms) error
-    :param pred_array: the predicted values
-    :param ref_array: the reference values
-    :return: the rms error
-    """
-    return np.sqrt(np.mean((pred_array - ref_array) ** 2))
-
-
-def mean_absolute_error(pred_array, ref_array):
-    """
-    Calculate mean absolute error
-    :param pred_array: the predicted values
-    :param ref_array: the reference values
-    :return: the mean absolute error
-    """
-    return np.mean(np.abs(pred_array - ref_array))
-
-
 def pearson_confidence(r, num, interval=0.95):
     """
     Calculate upper and lower 95% CI for a Pearson r (not R**2)
@@ -58,4 +38,4 @@ def max_possible_correlation(vals, error=0.6, method=pearsonr, cycles=1000):
         for val in vals:
             noisy_vals.append(val + np.random.normal(0, error))
         cor_list.append(method(vals, noisy_vals)[0])
-    return np.mean(cor_list)
+    return np.mean(cor_list), np.array(cor_list)
