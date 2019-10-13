119b7101b854571f8789c8564dcac1c8be29d93f
Pawel Gniewek <gniewko.pablo@gmail.com>
2019-10-12 20:44:48 -0700

    Some utility functions moved from the nb to the util.py

--- src/notebooks/utils.py
+++ src/notebooks/utils.py
@@ -6,6 +6,11 @@ import numpy as np
 from scipy.stats import pearsonr
 from scipy.stats import norm
 
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit import DataStructs
+from rdkit.ML.Cluster import Butina
+
 
 def pearson_confidence(r, num, interval=0.95):
     """
@@ -41,3 +46,90 @@ def max_possible_correlation(vals, error=0.6, method=pearsonr, cycles=1000):
             noisy_vals.append(val + np.random.normal(0, error))
         cor_list.append(method(vals, noisy_vals)[0])
     return np.mean(cor_list), np.array(cor_list)
+
+
+def get_statisicts(statistic, models_data_files, statistics_map, RESULTS_PATH):
+    """Read the statistics for each model
+    :argument:
+      statistic -- (string) models statistic
+    :return:
+      results -- (dict) a dictionary with statistic values (and standard deviations) for all available models.
+    """
+    results = {}
+    for filename in models_data_files:
+        with open('{}/{}'.format(RESULTS_PATH, filename), 'r') as fin:
+            for line in fin:
+                if line.startswith('#'):
+                    continue
+                pairs = line.rstrip('\n').split()
+
+                model_idx = statistics_map[statistic]
+                try:
+                    results[pairs[0]] = [float(pairs[2*model_idx+1]), float(pairs[2*model_idx+2])]
+                except ValueError as e:
+                    print(e)
+                    print("skipping the line")
+
+    return results
+
+
+def cluster_fps(fps, cutoff=0.2):
+    """ Given the set of fingerpring return set of clusters.
+    Similarity is calculated with Tanimoto metric.
+    :arguments:
+      fps (list) -- a list of fingerprints
+      cutoff (flat) -- a cutoff for clustering. Compounds in the same claster have diff in similarity less than the cutoff
+
+    :return:
+      cs (list) -- list of clusters. Each cluster is given by a tuple with compounds ids as they occure in fps. The firs id is for the cluster exemplars.
+    """
+    # first generate the distance matrix:
+    dists = []
+    nfps = len(fps)
+    for i in range(1, nfps):
+        sims = DataStructs.BulkTanimotoSimilarity(fps[i], fps[:i])
+        dists.extend([1 - x for x in sims])
+
+    # now cluster the data:
+    cs = Butina.ClusterData(dists, nfps, cutoff, isDistData=True)
+    return cs
+
+
+def get_data(fname):
+    vals = []
+    with open(fname, 'r') as fin:
+        for line in fin:
+            pairs = line.rstrip('\n').split(',')
+            vals.append(float(pairs[1]))
+
+    return vals
+
+
+def get_sim_data(ref_file, target_file):
+    ref_smiles = []
+    with open(ref_file, 'r') as fin:
+        for line in fin:
+            pairs = line.rstrip('\n').split(',')
+            ref_smiles.append(pairs[0])
+
+    target_smiles = []
+    with open(target_file, 'r') as fin:
+        for line in fin:
+            pairs = line.rstrip('\n').split(',')
+            target_smiles.append(pairs[0])
+
+    ref_fps = [AllChem.GetMorganFingerprintAsBitVect(Chem.MolFromSmiles(x), 3, nBits=1024) for x in ref_smiles]
+    target_fps = [AllChem.GetMorganFingerprintAsBitVect(Chem.MolFromSmiles(x), 3, nBits=1024) for x in target_smiles]
+
+    mol_sims = []
+    for i, fp_i in enumerate(target_fps):
+        min_sim = 0.0
+        for j, fp_j in enumerate(ref_fps):
+            if i == j:
+                continue
+            sim_ij = DataStructs.TanimotoSimilarity(fp_i, fp_j)
+            min_sim = max(min_sim, sim_ij)
+
+        mol_sims.append(min_sim)
+
+    return mol_sims
